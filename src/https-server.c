c6d43ab3afb7c2e47641f9f22142d17514f0118d
unknown <1021052838@qq.com>
2019-12-13 14:44:51 +0800

    merge uploadfile

--- src/https-server.c
+++ src/https-server.c
@@ -54,6 +54,7 @@
 #include <event2/buffer.h>
 #include <event2/util.h>
 #include <event2/keyvalq_struct.h>
+#include "template.h"
 
 #ifdef EVENT__HAVE_NETINET_IN_H
 #include <netinet/in.h>
@@ -86,7 +87,8 @@ typedef union {
  * any other callback.  Like any evhttp server callback, it has a simple job:
  * it must eventually call evhttp_send_error() or evhttp_send_reply().
  */
-
+void upload_get(struct evhttp_request *req, void *args);
+void upload_post(struct evhttp_request *req, void *args);
 
 // 处理get和post的回调方法 //
 // GET方法中的查询字符串（键值对）实际上是从URI中获得的
@@ -114,6 +116,12 @@ void deal_get(struct evhttp_request *req, void *args)
     }
     evbuffer_add_printf(evb, "You have sent a GET request to the server\r\n");
     evbuffer_add_printf(evb, "Request URI: %s\r\n", uri);
+    //upload
+    char * beginwith = strstr(uri, "/upload");
+    if (beginwith == uri){
+        upload_get(req,args);
+    }//upload end
+
     for (struct evkeyval *head = kvs.tqh_first; head != NULL; head = head->next.tqe_next)
     {
         evbuffer_add_printf(evb, "%s=%s\r\n", head->key, head->value);
@@ -126,7 +134,8 @@ void deal_get(struct evhttp_request *req, void *args)
 // 处理post请求,这里目前针对content-type 为x-www-form-urlencoded编码格式的请求，其他格式认为是不好的请求
 // TODO: analyse different types of the post body(such as form-data)
 void deal_post(struct evhttp_request *req, void *args)
-{   
+{
+    //TODO: segment fault when dealing with post upload
     struct evbuffer *evb = evbuffer_new();
     
     if (!evb)
@@ -221,6 +230,21 @@ int get_filename(const char* uri, char *file)
 /*文件上传和下载函数实现*/
 void do_upload_file(struct evhttp_request *req, void *args)
 {
+}
+
+void upload_get(struct evhttp_request *req, void *args){
+    struct evbuffer *evb = evbuffer_new();
+    if (!evb)
+    {
+        fprintf(stderr, "Couldn't create buffer\n");
+        return;
+    }
+    evbuffer_add_printf(evb, UPLOAD_HTML_TEMPLATE);
+    evhttp_send_reply(req, HTTP_OK, "OK", evb);
+    evbuffer_free(evb);
+}
+
+void upload_post(){
 
 }
 
